3bf8c1b62b1497f5f98ec9de6593a66c49f72223
Jade Allen Cook <jadeallencook@gmail.com>
2017-01-20 12:25:09 -0500

    ⛏ Complete blog page

--- ui/blog/index.php
+++ ui/blog/index.php
@@ -65,11 +65,50 @@
 
     <div id="page-container">
         <?php include_once('../partials/navbar.php') ?>
-        <!-- insert here -->
+            <!-- insert here -->
 
-        <?php include_once('../partials/footer.php') ?>
-        <?php include_once('../partials/modals.php') ?>
-        
+            <section id="header-page" class="header-margin-base">
+                <div class="skyline">
+                    <div data-offset="50" class="p1 parallax"></div>
+                    <div data-offset="25" class="p2 parallax"></div>
+                    <div data-offset="15" class="p3 parallax"></div>
+                    <div data-offset="8" class="p4 parallax"></div>
+                    <span class="cover"></span>
+                    <div class="container header-text">
+                        <div>
+                            <h1 class="title">Our Blogs</h1></div>
+                        <div>
+                            <h2 class="sub-title">Lorem ipsum dolor sit ats adipiscing elit justo</h2></div>
+                    </div>
+                </div>
+                <div id="breadcrumb">
+                    <div class="container">
+                        <ol class="breadcrumb">
+                            <li><a href="../"><i class="fa fa-home"></i></a></li>
+                            <li class="active">Blog</li>
+                        </ol>
+                    </div>
+                </div>
+                <!-- /#breadcrumb -->
+                <span class="cover"></span>
+            </section>
+            <!-- /#header -->
+
+            <section id="blog">
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-12 col-md-12" id="blogs-container">
+                            <h1>Loading...</h1>
+                        </div>
+                    </div>
+                </div>
+                
+            </section>
+
+            <?php include_once('../partials/footer.php') ?>
+                <?php include_once('../partials/modals.php') ?>
+
     </div>
     <!-- /#page-container -->
 
@@ -98,6 +137,7 @@
     <script src="../script/vendor/noui-slider/nouislider.all.min.js"></script>
     <!-- Range Slider -->
     <script src="../script/custom.js"></script>
+    <script src="../script/blog-full.js"></script>
     <!-- Custom	Script -->
 </body>
 
