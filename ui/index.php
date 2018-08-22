9db701454b4085dbb062c0bd1563666a20e10893
Jade Allen Cook <jadeallencook@gmail.com>
2018-08-21 22:29:35 -0600

    🐞 Fixed homepage console warnings and removed unused modal

--- ui/index.php
+++ ui/index.php
@@ -67,6 +67,7 @@
             <img src="images/logo-badge.png" id="loading-logo" />
             <br />
             <br />
+            <h2 id="loading-msg" style="color: #fff"></h2>
             <script>
                 var lines = new Array(
                     'Loading your dream vacation...',
@@ -74,7 +75,7 @@
                     'Sweeping off the welcome mat...',
                     'Looking forward to your visit...'
                 );
-                document.write('<h2 style="color: #fff">' + lines[Math.round(Math.random() * (lines.length - 1))] + '</h2>');
+                document.getElementById('loading-msg').innerText = lines[Math.round(Math.random() * (lines.length - 1))];
             </script>
         </div>
     </div>
@@ -128,42 +129,42 @@
         <section id="home-slide" class="header-margin-color-line">
             <div class="home-slider carousel" data-navigation=".home-slider-nav">
                 <div class="crsl-wrap">
-                    <figure class="crsl-item" id="banner-1" data-image="">
+                    <figure class="crsl-item" id="banner-1-image" data-image="">
                         <div class="container slider-box">
                             <div class="content">
-                                <h2></h2>
+                                <h2 id="banner-1-top-text"></h2>
                             </div>
                             <div class="content">
-                                <h1></h1>
+                                <h1 id="banner-1-big-text"></h1>
                             </div>
                             <div class="content">
-                                <h3></h3>
+                                <h3 id="banner-1-decription"></h3>
                             </div>
                         </div>
                     </figure>
-                    <figure class="crsl-item" id="banner-2" data-image="">
+                    <figure class="crsl-item" id="banner-2-image" data-image="">
                         <div class="container slider-box">
                             <div class="content">
-                                <h2></h2>
+                                <h2 id="banner-2-top-text"></h2>
                             </div>
                             <div class="content">
-                                <h1></h1>
+                                <h1 id="banner-2-big-text"></h1>
                             </div>
                             <div class="content">
-                                <h3></h3>
+                                <h3 id="banner-2-decription"></h3>
                             </div>
                         </div>
                     </figure>
-                    <figure class="crsl-item" id="banner-3" data-image="">
+                    <figure class="crsl-item" id="banner-3-image" data-image="">
                         <div class="container slider-box">
                             <div class="content">
-                                <h2></h2>
+                                <h2 id="banner-3-top-text"></h2>
                             </div>
                             <div class="content">
-                                <h1></h1>
+                                <h1 id="banner-3-big-text"></h1>
                             </div>
                             <div class="content">
-                                <h3></h3>
+                                <h3 id="banner-3-decription"></h3>
                             </div>
                         </div>
                     </figure>
@@ -360,10 +361,11 @@
                     </div>
                     <br />
                     <div class="row">
-                        <div class="hidden-xs col-md-9 credits-text">Copyright
+                        <div class="hidden-xs col-md-9 credits-text">Copyright <span id="footer-year"></span>
                             <script>
-                                document.write(new Date().getFullYear());
-                            </script> <b id="perfectlanding">Perfect Landing</b> | Developed By <b id="jadeallencook">Jade Allen Cook</b> | UI Designed By <b id="andylab">AndyLab</b></div>
+                                document.getElementById('footer-year').innerText = new Date().getFullYear();
+                            </script> 
+                            <b id="perfectlanding">Perfect Landing</b> | Developed By <b id="jadeallencook">Jade Allen Cook</b> | UI Designed By <b id="andylab">AndyLab</b></div>
                         <div class="col-md-3">
                             <ul class="social-icons">
                                 <li><a gdoc="facebook" target="_blank" href="#"><i class="fa fa-facebook"></i></a></li>
@@ -376,109 +378,6 @@
             </div>
         </footer>
 
-        <div class="modal fade login-modal" tabindex="-1" role="dialog" aria-hidden="true">
-            <div class="modal-dialog">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="fa fa-close"></i></button>
-                <div class="login-button-container">
-                    <a href="#" data-section="login"><i class="fa fa-user"></i></a>
-                    <a href="#" data-section="sign-in"><i class="fa fa-pencil-square-o"></i></a>
-                    <a href="#" data-section="recovery"><i class="fa fa-lock"></i></a>
-                    <a href="#" data-section="setting"><i class="fa fa-cog"></i></a>
-                </div>
-                <!-- ./login-button-container -->
-                <div class="form-container">
-                    <form method="post" action="#">
-                        <div id="login" class="box">
-                            <h2 class="title">Login in to your account</h2>
-                            <h3 class="sub-title">It is a breach of our terms and conditions to provide username and password details to unauthorised third parties. Unauthorised use may lead to suspension or termination.</h3>
-                            <div class="field">
-                                <input id="user-log" name="user-log" class="form-control" type="text" placeholder="Username or email">
-                                <i class="fa fa-user user"></i>
-                            </div>
-                            <div class="field">
-                                <input id="password-log" name="password-log" class="form-control" type="password" placeholder="Password">
-                                <i class="fa fa-ellipsis-h"></i>
-                            </div>
-                            <div class="field footer-form text-right">
-                                <span class="remember"><input class="labelauty" type="checkbox" data-labelauty="Keep me signed in" checked /></span>
-                                <button type="button" class="btn btn-reverse button-form">Reset</button>
-                                <button type="button" class="btn btn-default button-form">Login</button>
-                            </div>
-                        </div>
-                        <!-- ./login -->
-                        <div id="sign-in" class="box">
-                            <h2 class="title">Sign In</h2>
-                            <h3 class="sub-title">Create a Free account and discover how you can centralize all communication around a transaction, connect with clients, market your listings, and more. </h3>
-                            <div class="form-inline">
-                                <div class="form-group">
-                                    <input id="user-sign" name="user-sign" class="form-control input-inline margin-right" type="text" placeholder="Username">
-                                    <i class="fa fa-user user"></i>
-                                </div>
-                                <div class="form-group">
-                                    <input id="email-sign" class="form-control input-inline" type="text" name="email-sign" placeholder="Email">
-                                    <i class="fa fa-envelope-o"></i>
-                                </div>
-                            </div>
-                            <div class="field">
-                                <input id="password-sign" class="form-control" type="password" name="password-sign" placeholder="Password">
-                                <i class="fa fa-ellipsis-h"></i>
-                            </div>
-                            <div class="field">
-                                <input id="re-password-sign" class="form-control" type="password" name="re-password-sign" placeholder="Repeat password">
-                                <i class="fa fa-ellipsis-h"></i>
-                            </div>
-                            <div class="field footer-form text-right">
-                                <span class="remember"><input class="labelauty" type="checkbox" data-labelauty="I have read the privacy policy." checked /></span>
-                                <button type="button" class="btn btn-default button-form">Sign in</button>
-                            </div>
-                        </div>
-                        <!-- ./sign-in -->
-                        <div id="setting" class="box">
-                            <h2 class="title">Setting profile</h2>
-                            <h3 class="sub-title">Please note: You won't be able to change your name within the next 60 days. Make sure that you don't add any unusual capitalisation, punctuation, characters or random words. <a href="#">Learn more</a>.</h3>
-                            <div class="field">
-                                <input id="username-block" class="form-control" type="text" name="username-block" value="John Doe" disabled>
-                                <i class="fa fa-user user"></i>
-                            </div>
-                            <div class="field">
-                                <input id="email-setting" class="form-control" type="text" name="email-setting" value="administrator@prohome.com">
-                                <i class="fa fa-envelope-o"></i>
-                            </div>
-                            <div class="field">
-                                <input id="update-pass" class="form-control" type="password" name="update-pass" placeholder="New password">
-                                <i class="fa fa-ellipsis-h"></i>
-                            </div>
-                            <div class="field">
-                                <input id="update-repass" class="form-control" type="password" name="update-repass" placeholder="Repeat password">
-                                <i class="fa fa-ellipsis-h"></i>
-                            </div>
-                            <div class="field footer-form text-right">
-                                <button type="button" class="btn btn-reverse button-form">Cancel</button>
-                                <button type="button" class="btn btn-default button-form">Update</button>
-                            </div>
-                        </div>
-                        <!-- ./recovery -->
-                        <div id="recovery" class="box">
-                            <h2 class="title">Need a new password?</h2>
-                            <h3 class="sub-title">Enter your email address, and we’ll email you instructions to reset your password.</h3>
-                            <div class="field">
-                                <input id="recovery-email" class="form-control" type="text" name="recovery-email" placeholder="Your email">
-                                <i class="fa fa-envelope-o"></i>
-                            </div>
-                            <div class="field footer-form text-right">
-                                <button type="button" class="btn btn-default button-form">Recovery</button>
-                            </div>
-                        </div>
-                        <!-- ./recovery -->
-                    </form>
-                    <!-- ./form-container -->
-                </div>
-                <!-- ./login-button-container -->
-            </div>
-            <!-- /.modal-dialog -->
-        </div>
-        <!-- /.modal -->
-
         <div class="modal fade" id="modal-contact" tabindex="-1" role="dialog" aria-hidden="true">
             <div class="modal-dialog">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="fa fa-close"></i></button>
@@ -490,16 +389,17 @@
     <!-- /#page-container -->
 
     <!-- firebase -->
-    <script src="https://www.gstatic.com/firebasejs/5.4.0/firebase.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/5.3.0/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/5.3.0/firebase-database.js"></script>
     <script src="script/firebase-connect.js"></script>
-
+    <!-- cpm -->
+    <script src="script/cloud-property-manager.js"></script>
     <script src="script/jquery.min.js"></script>
     <!-- jQuery	(necessary for Bootstrap's JavaScript plugins) -->
     <script src="script/jquery-ui.min.js"></script>
     <!-- jQuery	UI is a	curated	set	of user	interface interactions,	effects, widgets, and themes -->
     <script src="script/bootstrap.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-
     <script src="script/vendor/mmenu/mmenu.min.all.js"></script>
     <!-- Menu Responsive -->
     <script src="script/vendor/animation-wow/wow.min.js"></script>
