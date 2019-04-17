2106a020dcd5b67d8fe54f81ec31fafb36724990
Jade Allen Cook <jadeallencook@gmail.com>
2019-04-16 20:41:04 -0600

    ⚙️ Exported newest build

---
+++ build/precache-manifest.c251e477da4030e48dfdde3ee9098d31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46396dd80e8a79107e17",
+    "url": "/Perfect-Landing/build/static/js/runtime~main.46396dd8.js"
+  },
+  {
+    "revision": "f4ef720b91004424e2a9",
+    "url": "/Perfect-Landing/build/static/js/main.f4ef720b.chunk.js"
+  },
+  {
+    "revision": "730204f7ebf157df1c4e",
+    "url": "/Perfect-Landing/build/static/js/2.730204f7.chunk.js"
+  },
+  {
+    "revision": "f4ef720b91004424e2a9",
+    "url": "/Perfect-Landing/build/static/css/main.907f065c.chunk.css"
+  },
+  {
+    "revision": "e6162572b6845e43acd2ae52de9f9b8f",
+    "url": "/Perfect-Landing/build/index.html"
+  }
+];
