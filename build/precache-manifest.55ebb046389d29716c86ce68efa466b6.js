6c239fd618454c30910a85d42bef14df213fd6e5
Jade Allen Cook <jadeallencook@gmail.com>
2020-01-26 20:24:39 -0800

    🚀 Exported production build

--- build/precache-manifest.55ebb046389d29716c86ce68efa466b6.js
+++ build/precache-manifest.55ebb046389d29716c86ce68efa466b6.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.42ac5946.js"
   },
   {
-    "revision": "5ef765ae1a2ef9435646",
-    "url": "/static/js/main.5ef765ae.chunk.js"
+    "revision": "203ef3a934bc4b671a87",
+    "url": "/static/js/main.203ef3a9.chunk.js"
   },
   {
     "revision": "a41b7ea48207d99f27d5",
     "url": "/static/js/2.a41b7ea4.chunk.js"
   },
   {
-    "revision": "5ef765ae1a2ef9435646",
-    "url": "/static/css/main.f88aee74.chunk.css"
+    "revision": "203ef3a934bc4b671a87",
+    "url": "/static/css/main.ccb712c2.chunk.css"
   },
   {
-    "revision": "35de0a4e23519fc9b2f6d6c942ca0f0a",
+    "revision": "67dba11810a757266957ddfa32a5aa29",
     "url": "/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c597151a56d5acb63a33b3fc1e6d2464.js"
+  "/precache-manifest.55ebb046389d29716c86ce68efa466b6.js"
 );
 
 workbox.clientsClaim();
